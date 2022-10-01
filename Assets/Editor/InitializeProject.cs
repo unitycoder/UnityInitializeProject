5564cc1610d3818d785531a3ae408d7f5d244569
mika <unitycoder@users.noreply.github.com>
2022-10-01 10:46:03 +0300

    add self-destruct after init, add skip self-destruct if run from menu item (while testing)

--- Assets/Editor/InitializeProject.cs
+++ Assets/Editor/InitializeProject.cs
@@ -12,11 +12,22 @@ namespace UnityLauncherProTools
         static string[] folders = new string[] { "Fonts", "Materials", "Models", "Prefabs", "Scenes", "Scripts", "Shaders", "Sounds", "Textures" };
 
         static string assetsFolder;
+        static bool deleteFile = true;
 
         [MenuItem("Tools/Initialize Project")]
+        public static void InitManually()
+        {
+            // called manually from menu, so dont delete file when testing
+            deleteFile = false;
+            Init();
+        }
+
+        // this method is called from launcher, without parameters, so delete is called
         public static void Init()
         {
             assetsFolder = Application.dataPath;
+
+            // TODO show window to select options for project init
 
             CreateFolders();
             // TODO adjust project settings, linear, company name
@@ -27,7 +38,13 @@ namespace UnityLauncherProTools
             // TODO add mainscene to build scenes list
             // TODO adjust quality settings (but only in mobile?)
 
-            // TODO self destruct this editor script file?
+            // self destruct this editor script file
+            if (deleteFile == true)
+            {
+                var scriptPath = Path.Combine(assetsFolder, "Scripts/Editor/InitializeProject.cs");
+                if (File.Exists(scriptPath)) File.Delete(scriptPath);
+                if (File.Exists(scriptPath + ".meta")) File.Delete(scriptPath + ".meta");
+            }
 
             // refresh folder
             AssetDatabase.Refresh();
