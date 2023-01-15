f70e71ae7ccda0b96a05a95e9775539e7f8390f3
mika <unitycoder@users.noreply.github.com>
2023-01-15 22:08:24 +0200

    add initial package cleanup (seems to work only 2020.3 and later), Settings: Clear skybox, remove skybox from cam, Add conditional: Sceneview settings only work for 2019.1 and later, Fix: Missing Scenes folder, if had disabled create folders

--- Assets/Editor/InitializeProject.cs
+++ Assets/Editor/InitializeProject.cs
@@ -2,10 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
+using System.Xml;
 using UnityEditor;
-using UnityEditor.PackageManager.UI;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
@@ -14,7 +13,10 @@ namespace UnityLauncherProTools
     public class InitializeProject : EditorWindow
     {
         // settings
-        static string[] folders = new string[] { "Fonts", "Materials", "Models", "Prefabs", "Scenes", "Scripts", "Shaders", "Sounds", "Textures" };
+        static string[] folders = new string[] { "Fonts", "Materials", "Models", "Plugins", "Prefabs", "Scenes", "Scripts", "Shaders", "Sounds", "Textures" };
+
+        static Dictionary<string, string> addPackages = new Dictionary<string, string>() { { "com.unity.ide.visualstudio", "2.0.17" } };
+        static string[] blackListedPackages = new string[] { "com.unity.modules.unityanalytics", "com.unity.modules.director", "com.unity.collab-proxy", "com.unity.ide.rider", "com.unity.ide.vscode", "com.unity.test-framework", "com.unity.timeline" };
 
         static InitializeProject window;
         static string assetsFolder;
@@ -22,6 +24,7 @@ namespace UnityLauncherProTools
 
         // settings
         static bool createFolders = true;
+        static bool updatePackages = true;
 
         [MenuItem("Tools/UnityLibrary/Initialize Project")]
         public static void InitManually()
@@ -44,6 +47,7 @@ namespace UnityLauncherProTools
             GUILayout.Space(10);
 
             Checkbox("Create Folders", ref createFolders);
+            Checkbox("updatePackages", ref updatePackages);
 
             GUILayout.Space(10);
             if (GUILayout.Button("Setup Project", GUILayout.Height(64))) SetupProject();
@@ -58,7 +62,7 @@ namespace UnityLauncherProTools
 
             if (createFolders) CreateFolders();
 
-            // TODO set these somewhere
+            // TODO set these somewhere globally?
             PlayerSettings.companyName = "Company";
             PlayerSettings.productName = "Project";
 
@@ -66,6 +70,10 @@ namespace UnityLauncherProTools
 
             // save scene
             var scenePath = "Assets/Scenes/Main.unity";
+            if (Directory.Exists(scenePath) == false)
+            {
+                Directory.CreateDirectory("Assets/Scenes");
+            }
             EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), scenePath);
 
             // add scene to build settings
@@ -73,15 +81,25 @@ namespace UnityLauncherProTools
             if (!string.IsNullOrEmpty(scenePath)) editorBuildSettingsScenes.Add(new EditorBuildSettingsScene(scenePath, true));
             EditorBuildSettings.scenes = editorBuildSettingsScenes.ToArray();
 
-            // TODO 2d/3d mode for editor
-            // TODO remove extra packages
-            // TODO setup light settings
-            // TODO adjust mainscene: camera pos, skybox off?
+            // TODO 2d/3d mode for editor?
+
+            UpdatePackages();
+
+            // skybox off from lighting settings
+            RenderSettings.skybox = null;
+
+            // skybox off from camera
+            Camera.main.clearFlags = CameraClearFlags.SolidColor;
+            // TODO set background color?
+
+            // reset camera pos
             Camera.main.transform.position = Vector3.zero;
 
             // disable editor camera easing and acceleration
+#if UNITY_2019_1_OR_NEWER
             SceneView.lastActiveSceneView.cameraSettings.easingEnabled = false;
             SceneView.lastActiveSceneView.cameraSettings.accelerationEnabled = false;
+#endif
 
             // GizmoUtility in 2022.1
             //GizmoUtility.SetGizmoEnabled(GizmoType.Move, true);
@@ -109,8 +127,9 @@ namespace UnityLauncherProTools
             // self destruct this editor script file
             if (deleteFile == true)
             {
-                // FIXME file is deleted, if reimport some script, while editorwindow is open
+                // FIXME in editor: file is deleted, if re-import some script, while editorwindow is open (resets deletefile var?)
                 var scriptPath = Path.Combine(assetsFolder, "Editor/InitializeProject.cs");
+                Debug.Log("Deleting init script: " + scriptPath);
                 if (File.Exists(scriptPath)) File.Delete(scriptPath);
                 if (File.Exists(scriptPath + ".meta")) File.Delete(scriptPath + ".meta");
             }
@@ -124,6 +143,80 @@ namespace UnityLauncherProTools
         }
 
 
+        static void UpdatePackages()
+        {
+            // check if packages.json exists
+            var packagesPath = Path.Combine(assetsFolder, "../Packages/manifest.json");
+            if (!File.Exists(packagesPath)) return;
+
+            var json = File.ReadAllText(packagesPath);
+
+            // NOTE this seems to work in 2020.3 and later?
+            var jsonConvertType = Type.GetType("Newtonsoft.Json.JsonConvert, Newtonsoft.Json");
+            if (jsonConvertType != null)
+            {
+                jsonConvertType = Assembly.Load("Newtonsoft.Json").GetType("Newtonsoft.Json.JsonConvert");
+            }
+            IJsonSerializer jsonSerializer;
+            if (jsonConvertType != null)
+            {
+                jsonSerializer = new NewtonsoftJsonSerializer(jsonConvertType);
+            }
+            else
+            {
+                jsonSerializer = new DefaultJsonSerializer();
+            }
+
+            // do we have real newtonsoft
+            Type type = Type.GetType("Newtonsoft.Json.JsonConvert, Newtonsoft.Json");
+            if (type != null)
+            {
+                //Debug.Log("We have Newtonsoft.Json");
+                var fromJson = jsonSerializer.Deserialize<DependenciesManifest>(json);
+
+                for (int i = fromJson.dependencies.Count; i > -1; i--)
+                {
+                    for (int k = 0; k < blackListedPackages.Length; k++)
+                    {
+                        if (fromJson.dependencies.ContainsKey(blackListedPackages[k]))
+                        {
+                            fromJson.dependencies.Remove(blackListedPackages[k]);
+                            //Debug.Log("Removed " + blackListedPackages[k]);
+                        }
+                    }
+                }
+
+                // add wanted packages, if missing
+                foreach (KeyValuePair<string, string> item in addPackages)
+                {
+                    // TODO check if want to increase version number?
+                    if (fromJson.dependencies.ContainsKey(item.Key) == false)
+                    {
+                        fromJson.dependencies.Add(item.Key, item.Value);
+                        Debug.Log("Added " + item.Key);
+                    }
+                    else
+                    {
+                        //Debug.Log("Already contains " + item.Key);
+                    }
+                }
+
+                // TODO add pretty print
+                var toJson = jsonSerializer.Serialize(fromJson);
+                // FIXME temporary pretty print, by adding new lines and tabs
+                toJson = toJson.Replace(",", ",\n");
+                toJson = toJson.Replace("{", "{\n");
+                toJson = toJson.Replace("}", "\n}");
+                toJson = toJson.Replace("\"dependencies", "\t\"dependencies");
+                toJson = toJson.Replace("\"com.", "\t\t\"com.");
+                //Debug.Log(toJson);
+                File.WriteAllText(packagesPath, toJson);
+            }
+            else
+            {
+                Debug.Log("Newtonsoft.Json is not available, cannot remove packages..");
+            }
+        }
 
         // toggle with clickable label text
         static void Checkbox(string label, ref bool value)
@@ -139,11 +232,58 @@ namespace UnityLauncherProTools
             // create each folder if it doesnt exists
             foreach (string folder in folders)
             {
-                if (!Directory.Exists(assetsFolder + "/" + folder))
+                if (!Directory.Exists(assetsFolder + " / " + folder))
                 {
                     Directory.CreateDirectory(assetsFolder + "/" + folder);
                 }
             }
+        }
+    }
+
+    // manifest.json
+    public class DependenciesManifest
+    {
+        public Dictionary<string, string> dependencies { get; set; }
+    }
+
+    public interface IJsonSerializer
+    {
+        string Serialize(object obj);
+        T Deserialize<T>(string json);
+    }
+
+    public class DefaultJsonSerializer : IJsonSerializer
+    {
+        public string Serialize(object obj)
+        {
+            return "default serializer";
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            return default(T);
+        }
+    }
+
+    public class NewtonsoftJsonSerializer : IJsonSerializer
+    {
+        private readonly Type jsonConvertType;
+
+        public NewtonsoftJsonSerializer(Type jsonConvertType)
+        {
+            this.jsonConvertType = jsonConvertType;
+        }
+
+        public string Serialize(object obj)
+        {
+            var serializeMethod = jsonConvertType.GetMethod("SerializeObject", new Type[] { typeof(object) });
+            return (string)serializeMethod.Invoke(null, new object[] { obj });
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            var deserializeMethod = jsonConvertType.GetMethod("DeserializeObject", new Type[] { typeof(string), typeof(Type) });
+            return (T)deserializeMethod.Invoke(null, new object[] { json, typeof(T) });
         }
     }
 }
