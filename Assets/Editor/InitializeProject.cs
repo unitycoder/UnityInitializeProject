aaf286a68431c4668bfd827c3c06073b0c5ad6fc
mika <unitycoder@users.noreply.github.com>
2023-10-11 21:46:02 +0300

    add "com.unity.visualscripting" to blacklisted packages, increment VS plugin version

--- Assets/Editor/InitializeProject.cs
+++ Assets/Editor/InitializeProject.cs
@@ -15,8 +15,8 @@ namespace UnityLauncherProTools
         // settings
         static string[] folders = new string[] { "Fonts", "Materials", "Models", "Plugins", "Prefabs", "Scenes", "Scripts", "Shaders", "Sounds", "Textures" };
 
-        static Dictionary<string, string> addPackages = new Dictionary<string, string>() { { "com.unity.ide.visualstudio", "2.0.20" } };
-        static string[] blackListedPackages = new string[] { "com.unity.modules.unityanalytics", "com.unity.modules.director", "com.unity.collab-proxy", "com.unity.ide.rider", "com.unity.ide.vscode", "com.unity.test-framework", "com.unity.timeline" };
+        static Dictionary<string, string> addPackages = new Dictionary<string, string>() { { "com.unity.ide.visualstudio", "2.0.22" } };
+        static string[] blackListedPackages = new string[] { "com.unity.modules.unityanalytics", "com.unity.modules.director", "com.unity.collab-proxy", "com.unity.ide.rider", "com.unity.ide.vscode", "com.unity.test-framework", "com.unity.timeline", "com.unity.visualscripting" };
 
         static InitializeProject window;
         static string assetsFolder;
